fcf9a708f06e1fa629a0743463818d849b44c54c
Bill Wheatley <bwheatle@redhat.com>
2020-01-15 22:56:30 -0500

    Making use of relitive paths in common script correct with a cd

--- bootstrap-apt.sh
+++ bootstrap-apt.sh
@@ -6,4 +6,5 @@ sudo apt-get -y install ansible git sshpass
 #clone from GIT
 git clone https://github.com/billwheatley/provision-desktop.git
 
-./provision-desktop/bootstrap-common.sh
+cd provision-desktop
+./bootstrap-common.sh
--- bootstrap-dnf.sh
+++ bootstrap-dnf.sh
@@ -5,6 +5,7 @@ sudo dnf -y install ansible git sshpass
 #clone from GIT
 git clone https://github.com/billwheatley/provision-desktop.git
 
-./provision-desktop/bootstrap-common.sh
+cd provision-desktop
+./bootstrap-common.sh
 
 
