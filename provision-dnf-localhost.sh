6ca5a5974c1bb3a1e1cb4917976227a603b91f46
Bill Wheatley <bwheatle@redhat.com>
2020-01-15 23:52:52 -0500

    fixing rpm fusion repos task

--- provision-dnf-localhost.sh
+++ provision-dnf-localhost.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 LOCAL_USER=${USER}
-sudo ansible-playbook provision-dnf-desktop.yaml --extra-vars='for_user="${LOCAL_USER}" fedora_version="`rpm -E %fedora`"'
+FEDORA_VERSION=`rpm -E %fedora`
+sudo ansible-playbook provision-dnf-desktop.yaml --extra-vars='for_user="${LOCAL_USER}" fedora_version="${FEDORA_VERSION}"'
