ec88a5a2d87435d0fd9d5fe45f69ce3dc2ac662c
Bill Wheatley <bwheatle@redhat.com>
2020-02-24 15:39:31 -0500

    adding jk package and verbose output to ansible-playbook command

--- provision-apt-localhost.sh
+++ provision-apt-localhost.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 LOCAL_USER=${USER}
-sudo ansible-playbook provision-apt-desktop.yaml --extra-vars="for_user=${LOCAL_USER}"
+sudo ansible-playbook provision-apt-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER}"
--- provision-dnf-localhost.sh
+++ provision-dnf-localhost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LOCAL_USER=${USER}
 FEDORA_VERSION=`rpm -E %fedora`
-sudo ansible-playbook provision-dnf-desktop.yaml --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION}"
+sudo ansible-playbook provision-dnf-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION}"
