65db69b94616ba6526b574b80fed3854b24eb2b6
Bill Wheatley <bwheatle@redhat.com>
2020-05-14 16:16:51 -0400

    Pop (Ubuntu) 20.04 no longer has plain python executable and that causes strange missleading errors with apt module adding logic to bootstrap to link to python3 if plain python is not present

--- bootstrap-common.sh
+++ bootstrap-common.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Makr Roles-dir
+# Make sure plain 'python' is in path (anisble does not do will without it)
+if [ ! `which python` ]; then
+  PYTHON3_LOC=$(which python3)
+  PYTHON_LOC=$(dirname $PYTHON3_LOC)/python
+  echo Need to Link $PYTHON3_LOC $PYTHON_LOC [using sudo]
+  sudo ln -s $PYTHON3_LOC $PYTHON_LOC
+fi
+
+# Make Roles-dir
 mkdir ../desktop-roles
 
 # TODO get roles from git
 
-#TODO cd into git dir
 #Call key setup playbook
 ansible-playbook key-setup.yaml --extra-vars='localhost_user="${USER}" ansible_user_dir="${HOME}"'
 
-# This is not working on nas
-# ssh-copy-id -i ~/.ssh/id_rsa.pub bwheatley@nas.local
+# TODO This is not working on nas
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER}@nas.local
 #changed /etc/ssh/sshd_config on the server contains PubkeyAuthentication yes. Remember to restart the sshd process on the server: nohup synoservicectl --restart sshd &
