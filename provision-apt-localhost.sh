7244e4e1534ba822118b2a304341e8990ac8ea16
Bill Wheatley <bwheatle@redhat.com>
2020-06-30 22:56:20 -0400

    renaming requirements.yml to requirements.yaml

--- provision-apt-localhost.sh
+++ provision-apt-localhost.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LOCAL_USER=${USER}
-ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yaml
 sudo ansible-playbook provision-apt-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER}"
--- provision-dnf-localhost-no-pkg-update.sh
+++ provision-dnf-localhost-no-pkg-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOCAL_USER=${USER}
 FEDORA_VERSION=`rpm -E %fedora`
-ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yaml
 sudo ansible-playbook provision-dnf-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION}" --skip-tags "pkg_update"
