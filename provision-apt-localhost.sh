5f81014afae10a43f40eccef48aecd443fbe56bf
Bill Wheatley <bwheatle@redhat.com>
2020-06-30 22:54:48 -0400

    Adding golang role to both dnf and apt, also adding requirements.yml pumbing

--- provision-apt-localhost.sh
+++ provision-apt-localhost.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 LOCAL_USER=${USER}
+ansible-galaxy install -r requirements.yml
 sudo ansible-playbook provision-apt-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER}"
--- provision-dnf-localhost-no-pkg-update.sh
+++ provision-dnf-localhost-no-pkg-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 LOCAL_USER=${USER}
 FEDORA_VERSION=`rpm -E %fedora`
+ansible-galaxy install -r requirements.yml
 sudo ansible-playbook provision-dnf-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION}" --skip-tags "pkg_update"
