44753500f9be66675eddec1796d0cfe83cb8d667
Bill Wheatley <bill_wheatley@mailcan.com>
2020-09-03 17:44:53 -0400

    adding use of oc-role

--- provision-fedora-localhost-no-pkg-update.sh
+++ provision-fedora-localhost-no-pkg-update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 LOCAL_USER=${USER}
 FEDORA_VERSION=`rpm -E %fedora`
-PACKAGE_DIR=$HOME/automated-install/rpm
+
+AUTOMATED_INSTALL_HOME=$HOME/automated-install
+PACKAGE_DIR=$AUTOMATED_INSTALL_HOME/rpm
+OC_ARCHIVE=$AUTOMATED_INSTALL_HOME/archive/oc.tar.gz
+
 ansible-galaxy install -r requirements.yaml
-sudo ansible-playbook provision-fedora-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION} package_dir=${PACKAGE_DIR}" --skip-tags "pkg_update"
+sudo ansible-playbook provision-fedora-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION} package_dir=${PACKAGE_DIR} oc_archive=${OC_ARCHIVE}" --skip-tags "pkg_update"
--- provision-fedora-localhost.sh
+++ provision-fedora-localhost.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 LOCAL_USER=${USER}
 FEDORA_VERSION=`rpm -E %fedora`
-PACKAGE_DIR=$HOME/automated-install/rpm
+AUTOMATED_INSTALL_HOME=$HOME/automated-install
+PACKAGE_DIR=$AUTOMATED_INSTALL_HOME/rpm
+OC_ARCHIVE=$AUTOMATED_INSTALL_HOME/archive/oc.tar.gz
+
 ansible-galaxy install -r requirements.yaml
-sudo ansible-playbook provision-fedora-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION} package_dir=${PACKAGE_DIR}"
+sudo ansible-playbook provision-fedora-desktop.yaml -v --extra-vars="for_user=${LOCAL_USER} fedora_version=${FEDORA_VERSION} package_dir=${PACKAGE_DIR} oc_archive=${OC_ARCHIVE}"
