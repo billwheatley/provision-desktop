73f41a27f0998469d2871079e89fc093dde70c49
Bill Wheatley <bill_wheatley@mailcan.com>
2020-09-08 23:25:22 -0400

    Adding first part of provision script cleanup

---
+++ bin/which-pkg-mgr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Figures out what package manager the system has, currently supported: dnf, yum, apt-get, NA
+
+if [ -x "$(command -v apt-get)" ]; then 
+    echo apt-get
+elif [ -x "$(command -v dnf)" ]; then 
+    echo dnf
+elif [ -x "$(command -v yun)" ]; then 
+    echo yum
+else 
+    echo NA
+    exit 1
+fi
