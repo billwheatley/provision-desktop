a9d8638556bd34ebe4e0db25609a036bec96d50f
Bill Wheatley <bill_wheatley@mailcan.com>
2020-10-20 23:16:20 -0400

    cleanup Removing old commented out code

--- bootstrap-common.sh
+++ bootstrap-common.sh
@@ -17,6 +17,3 @@ mkdir -p $HOME/automated-install/archive
 #Call key setup playbook
 ansible-playbook key-setup.yaml --extra-vars='localhost_user="${USER}" ansible_user_dir="${HOME}"'
 
-# TODO This is not working on nas
-# ssh-copy-id -i ~/.ssh/id_rsa.pub ${USER}@nas.local
-#changed /etc/ssh/sshd_config on the server contains PubkeyAuthentication yes. Remember to restart the sshd process on the server: nohup synoservicectl --restart sshd &
