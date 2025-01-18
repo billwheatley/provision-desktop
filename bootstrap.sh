7dcbfb5e9eeb5545a22a17924ee14a57e43fcce0
Bill Wheatley <bill_wheatley@mailcan.com>
2025-01-18 00:37:37 -0500

    Updating bootstrap for OM

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+# For Open Mandriva repo file editing
+safe_edit_repo_file() {
+   grep -n "enabled" /etc/yum.repos.d/openmandriva-rolling-x86_64.repo |grep $1 >/dev/null
+   if [ $? -eq 0 ]; then
+       sed_exp=`echo $1`"s/.*/enabled=1/"
+       sudo sed -i $sed_exp /etc/yum.repos.d/openmandriva-rolling-x86_64.repo 
+   else
+       echo Unexpected Repo file format [/etc/yum.repos.d/openmandriva-rolling-x86_64.repo], expecting enabled flag on line $1
+       echo ERROR: Script can not enable required Repo, this script needs to be fixed
+       exit 1
+   fi
+}
+
 if [ -x "$(command -v apt-get)" ]; then 
     sudo apt-get update
     sudo apt-get -y install ansible git sshpass
 elif [ -x "$(command -v dnf)" ]; then 
+    if [ -f /etc/openmandriva-release ]; then
+	echo Open Mandrivia Detected, additional repos need to be turned on now
+	sudo cp /etc/yum.repos.d/openmandriva-rolling-x86_64.repo /etc/yum.repos.d/openmandriva-rolling-x86_64.repo.backup
+	echo Backup of repo file made if this fails: /etc/yum.repos.d/openmandriva-rolling-x86_64.repo.backup
+	# Doing this by Line number
+	safe_edit_repo_file "59"
+	safe_edit_repo_file "107"
+	safe_edit_repo_file "155"
+    fi
     sudo dnf -y install ansible git sshpass
 elif [ -x "$(command -v yum)" ]; then 
     sudo yum -y install ansible git sshpass
